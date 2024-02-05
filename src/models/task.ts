fc6b5d330a6e841cf611119bed5890288e288a7f
Madhur Mittal <madhurmittal55@gmail.com>
2024-02-05 13:18:40 +0530

    feat: models

--- src/models/task.ts
+++ src/models/task.ts
@@ -1,6 +1,17 @@
-import { Schema } from 'mongoose';
+import mongoose, { Document, Schema } from 'mongoose';
 
-const taskSchema = new Schema(
+const statusEnum = ['Pending', 'In Progress', 'Completed'];
+
+export interface TypeTask extends Document {
+    title: string;
+    content: string;
+    status: string;
+    userId: mongoose.Types.ObjectId;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const taskSchema = new Schema<TypeTask>(
     {
         title: {
             type: String,
@@ -12,9 +23,22 @@ const taskSchema = new Schema(
             required: true,
             minLength: [1, 'Content must have at least 1 characters'],
         },
-        
+        status: {
+            type: String,
+            enum: statusEnum,
+            required: true,
+            default: statusEnum[0],
+        },
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'users',
+            required: true,
+        },
     },
     {
         timestamps: true,
     },
 );
+
+export const Task =
+    mongoose.models.Task || mongoose.model<TypeTask>('Task', taskSchema);
