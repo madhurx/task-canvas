3c25ef3433e3f4b11e5a8b9b6df4511fae0ed7bc
Madhur Mittal <madhurmittal55@gmail.com>
2024-02-12 17:56:35 +0530

    changes

--- src/service/taskService.ts
+++ src/service/taskService.ts
@@ -1,6 +1,14 @@
 import { httpAxios } from '@/helper/httpHelper';
 
-export async function addTask(task) {
+interface TaskType {
+    title: String;
+    content: String;
+    status: String;
+    reminderDate: String;
+    userId: String;
+}
+
+export async function addTask(task: TaskType) {
     const result = await httpAxios
         .post('/api/tasks', task)
         .then((response) => response.data);
