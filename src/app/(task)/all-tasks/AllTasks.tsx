01acbd919e35ddd3e86ac6e3d15d3982e0f761ab
Madhur Mittal <madhurmittal55@gmail.com>
2024-02-26 17:50:00 +0530

    feat: all-tasks table

--- src/app/(task)/all-tasks/AllTasks.tsx
+++ src/app/(task)/all-tasks/AllTasks.tsx
@@ -3,25 +3,28 @@
 import AuthContext from '@/Context/AuthContext';
 import { getUserTasks } from '@/service/taskService';
 import { useContext, useEffect, useState } from 'react';
+import { DataTable } from './data-table';
+import { columns } from './columns';
 
-interface TaskType {
+export type TypeTasksTable = {
     content: string;
     createdAt: Date;
     status: string;
     title: string;
     // reminderDate: Date;
     _id: string;
-}
+};
 
 const AllTasks = () => {
-    // const [task, setTask] = useState<TaskType[]>([]);
-    const [task, setTask] = useState<any>('B');
+    const [task, setTask] = useState<TypeTasksTable[]>([]);
+    // const [task, setTask] = useState<any>('B');
     const context = useContext(AuthContext);
 
     const loadTasks = async (userId: string) => {
         try {
             const tasks = await getUserTasks(userId);
-            // setTask((prevTasks ) => [...prevTasks , ...tasks.data]);
+            setTask(tasks.data);
+            // setTask("asd")
             console.log(tasks.data);
             console.log(task);
         } catch (error) {}
@@ -30,11 +33,15 @@ const AllTasks = () => {
         if (context.user) {
             loadTasks(context.user?.data?._id);
         }
-        console.log(task);
-        setTask('A');
+        // setTask('A');
     }, [context.user]);
 
-    return <div>AllTask</div>;
+    return (
+        <div className="container mx-auto py-10">
+            {/* {task} */}
+            <DataTable columns={columns} data={task} />
+        </div>
+    );
 };
 
 export default AllTasks;
--- src/app/(task)/all-tasks/columns.tsx
+++ src/app/(task)/all-tasks/columns.tsx
@@ -1,25 +1,128 @@
 'use client';
 
 import { ColumnDef } from '@tanstack/react-table';
+import { TypeTasksTable } from './AllTasks';
+import moment from 'moment';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { ArrowUpDown, MoreHorizontal } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
 
-export type Payment = {
-    id: string;
-    amount: number;
-    status: 'pending' | 'processing' | 'success' | 'failed';
-    email: string;
-};
-
-export const columns: ColumnDef<Payment>[] = [
+export const columns: ColumnDef<TypeTasksTable>[] = [
+    {
+        id: 'select',
+        header: ({ table }) => (
+            <Checkbox
+                checked={
+                    table.getIsAllPageRowsSelected() ||
+                    (table.getIsSomePageRowsSelected() && 'indeterminate')
+                }
+                onCheckedChange={(value: any) =>
+                    table.toggleAllPageRowsSelected(!!value)
+                }
+                aria-label="Select all"
+            />
+        ),
+        cell: ({ row }) => (
+            <Checkbox
+                checked={row.getIsSelected()}
+                onCheckedChange={(value: any) => row.toggleSelected(!!value)}
+                aria-label="Select row"
+            />
+        ),
+        enableSorting: false,
+        enableHiding: false,
+    },
+    {
+        accessorKey: 'title',
+        header: () => <div className="text-left">Task</div>,
+    },
+    {
+        accessorKey: 'content',
+        header: 'Task Description',
+    },
     {
         accessorKey: 'status',
-        header: 'Status',
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() =>
+                        column.toggleSorting(column.getIsSorted() === 'asc')
+                    }
+                >
+                    Task status
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            );
+        },
+        cell: ({ row }) => {
+            const status = row.getValue('status');
+            return <div className="pl-4 font-medium">{status as string}</div>;
+        },
     },
     {
-        accessorKey: 'email',
-        header: 'Email',
+        accessorKey: 'createdAt',
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() =>
+                        column.toggleSorting(column.getIsSorted() === 'asc')
+                    }
+                >
+                    Created Date
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            );
+        },
+
+        cell: ({ row }) => {
+            const date = row.getValue('createdAt');
+            const formatted = moment(date as Date)
+                .utc()
+                .format('DD/MM/YY');
+            return <div className="pl-4 font-medium">{formatted}</div>;
+        },
     },
     {
-        accessorKey: 'amount',
-        header: 'Amount',
+        id: 'actions',
+        enableHiding: false,
+        cell: ({ row }) => {
+            const actionOptions = row.original;
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem
+                            onClick={() =>
+                                navigator.clipboard.writeText(actionOptions._id)
+                            }
+                        >
+                            Copy payment ID
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>View customer</DropdownMenuItem>
+                        <DropdownMenuItem>
+                            View payment details
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            );
+        },
     },
 ];
